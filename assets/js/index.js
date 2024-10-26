27e365b1353a191d4dd083717f74cd1e3e4c085c
BinhPhuc <pham.phuc.binh.271106@gmail.com>
2024-10-26 14:15:58 +0700

    commit

---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+let careerStoreBtn = document.getElementById('career-store-btn');
+
+function handleCareerStore() {
+    window.location.href="career-store.html";
+}
+
+let viewJob1DetailsBtn = document.getElementById('job1');
+
+function handleJob1Details() {
+    window.location.href= "job_1_details.html";
+}
+
+let viewJob2DetailsBtn = document.getElementById('job2');
+
+function handleJob2Details() {
+    window.location.href= "job_2_details.html";
+}
+
+let viewJob3DetailsBtn = document.getElementById('job3');
+
+function handleJob3Details() {
+    window.location.href= "job_3_details.html";
+}
+
+function handleCareerOffice() {
+    window.location.href= "career-office.html";
+}
+
+let viewJob11DetailsBtn = document.getElementById('job11');
+
+function handleJob11Details() {
+    window.location.href= "job_11_details.html";
+}
+
+let viewJob22DetailsBtn = document.getElementById('job22');
+
+function handleJob22Details() {
+    window.location.href= "job_22_details.html";
+}
+
+let viewJob33DetailsBtn = document.getElementById('job33');
+
+function handleJob33Details() {
+    window.location.href= "job_33_details.html";
+}
